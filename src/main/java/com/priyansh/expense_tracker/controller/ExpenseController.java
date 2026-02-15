05f53a673c51e25d5c1265c6221f1c307efd9997
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-15 14:59:14 +0530

    feat: add the route to create an expense record

--- src/main/java/com/priyansh/expense_tracker/controller/ExpenseController.java
+++ src/main/java/com/priyansh/expense_tracker/controller/ExpenseController.java
@@ -1,4 +1,28 @@
 package com.priyansh.expense_tracker.controller;
 
+import com.priyansh.expense_tracker.dto.CreateExpenseRequest;
+import com.priyansh.expense_tracker.entity.Expense;
+import com.priyansh.expense_tracker.service.ExpenseService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/expenses")
 public class ExpenseController {
+    public final ExpenseService expenseService;
+    public ExpenseController(ExpenseService expenseService) {
+        this.expenseService = expenseService;
+    }
+    @PostMapping
+    public ResponseEntity<Expense>createExpense(
+            @Valid @RequestBody CreateExpenseRequest request){
+        Expense expense = expenseService.createExpense(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(expense);
+    }
 }
