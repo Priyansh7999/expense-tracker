ef6996c833833eff34f8f3430da58e1987bf37f8
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-15 13:19:06 +0530

    feat: write sql querry to add to seed data into database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO expense_categories (id, name) VALUES (1, 'FOOD');
+INSERT INTO expense_categories (id, name) VALUES (2, 'TRANSPORT');
+INSERT INTO expense_categories (id, name) VALUES (3, 'RENT');
+INSERT INTO expense_categories (id, name) VALUES (4, 'HEALTHCARE');
+INSERT INTO expense_categories (id, name) VALUES (5, 'EMI');
+INSERT INTO expense_categories (id, name) VALUES (6, 'OTHER');
