7b3965c23a4248791727cac89aa27a691e272f1e
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-15 15:41:27 +0530

    feat: add expection to handle invalid request

--- src/main/java/com/priyansh/expense_tracker/exception/GlobalExceptionHandler.java
+++ src/main/java/com/priyansh/expense_tracker/exception/GlobalExceptionHandler.java
@@ -1,10 +1,12 @@
 package com.priyansh.expense_tracker.exception;
 
+import com.priyansh.expense_tracker.entity.PaymentMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -32,6 +34,7 @@ public class GlobalExceptionHandler {
                 .body(errors);
     }
 
+
     // Resource Not Found
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<Map<String, String>> handleNotFound(
@@ -52,6 +55,23 @@ public class GlobalExceptionHandler {
         Map<String, String> error = new HashMap<>();
         error.put("message",
                 "Invalid value for paymentMethod. Allowed values: CASH, CREDIT_CARD, DEBIT_CARD, UPI");
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(error);
+    }
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<Map<String, String>> handleEnumError(
+            MethodArgumentTypeMismatchException ex) {
+
+        Map<String, String> error = new HashMap<>();
+
+        if (ex.getRequiredType() == PaymentMethod.class) {
+            error.put("message",
+                    "Invalid payment method. Allowed values: CASH, CREDIT_CARD, DEBIT_CARD, UPI");
+        } else {
+            error.put("message", "Invalid request parameter");
+        }
 
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
