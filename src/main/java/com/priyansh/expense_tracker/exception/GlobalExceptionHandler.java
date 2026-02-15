396fb628886fe339414f2b09621cf9dc715574af
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-15 14:59:59 +0530

    feat: add Global Exception Handler

--- src/main/java/com/priyansh/expense_tracker/exception/GlobalExceptionHandler.java
+++ src/main/java/com/priyansh/expense_tracker/exception/GlobalExceptionHandler.java
@@ -1,4 +1,72 @@
 package com.priyansh.expense_tracker.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    // Validation Errors
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(
+            MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult()
+                .getFieldErrors()
+                .forEach(error ->
+                        errors.put(
+                                error.getField(),
+                                error.getDefaultMessage()
+                        ));
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errors);
+    }
+
+    // Resource Not Found
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleNotFound(
+            ResourceNotFoundException ex) {
+
+        Map<String, String> error = new HashMap<>();
+        error.put("message", ex.getMessage());
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(error);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Map<String, String>> handleEnumError(
+            HttpMessageNotReadableException ex) {
+
+        Map<String, String> error = new HashMap<>();
+        error.put("message",
+                "Invalid value for paymentMethod. Allowed values: CASH, CREDIT_CARD, DEBIT_CARD, UPI");
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(error);
+    }
+
+    // Generic Exception
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, String>> handleGeneric() {
+
+        Map<String, String> error = new HashMap<>();
+        error.put("message", "Something went wrong");
+
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(error);
+    }
 }
